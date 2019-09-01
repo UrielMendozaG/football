428fd94e64210053088f4d6b3e486e2405ec6af1
vumg <ab17100222@gmail.com>
2019-09-01 18:52:51 -0500

    double tapp fixed

--- js/index.js
+++ js/index.js
@@ -1,14 +1,44 @@
 const bola = document.querySelector(".ball-run");
+const score = document.getElementById("score");
+var points = 0;
 
-function esperarTiempo() {
-  setTimeout(function() {
-    bola.classList.remove("ball-run");
+ function esperarTiempo() {
+   setTimeout(function() {
+     bola.classList.remove("ball-run");
 
-    bola.addEventListener("click", function() {
-      bola.classList.toggle("animation");
-      
+     bola.addEventListener("click", function() {
+       bola.classList.toggle("animation");
+     });
+
+     bola.addEventListener("dblclick", function() {
+       points++;
+       score.innerHTML = "<h2>"+points+"</h2>";
+     });
+
+     bola.addEventListener("mouseover", function() {
+      bola.style.cursor = "pointer";
     });
-  }, 1000);
-}
 
-esperarTiempo();
+    
+   }, 1000);
+ }
+
+document.getElementById("double-tap").addEventListener("touchstart", tapHandler);
+
+var tapedTwice = false;
+
+function tapHandler(event) {
+    if(!tapedTwice) {
+        tapedTwice = true;
+        setTimeout( function() { tapedTwice = false; }, 300 );
+        return false;
+    }
+    event.preventDefault();
+    //action on double tap goes below
+    points++;
+    score.innerHTML = "<h2>"+points+"</h2>";
+    bola.classList.toggle("animation");
+ }
+   
+
+ esperarTiempo();
